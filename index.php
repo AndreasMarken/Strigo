7a5ef5a9f13d3944fc374bfef2471a1bcd861415
AndreasMarken <78914842+AndreasMarken@users.noreply.github.com>
2021-02-16 09:00:52 +0100

    Lastet opp førsteutkast til Index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once '../shortcuts_php/knapper.php' ; ?>
+
+  <img src="../bilder/bilde1.png">
+
+  <img src="../bilder/bilde2.png">
+
+      </div> <!-- Body-->
+
+  </body>
+
+</html>

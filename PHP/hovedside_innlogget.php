8f9bc00a080576ff798d19a87d09acb09293fa1e
Andreas <Andreas.Marken@gmail.com>
2021-02-25 15:09:07 +0100

    Hovedside_innlogget fullført. Mangler bare å fullføre linker til ny side

--- PHP/hovedside_innlogget.php
+++ PHP/hovedside_innlogget.php
@@ -6,7 +6,7 @@ require_once '../shortcuts_php/kobling.php'; ?>
     <title>Strigo</title>
 
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../CSS/index.css">
+    <link rel="stylesheet" href="../CSS/hovedside_innlogget.css">
 
   </head>
 
@@ -23,9 +23,7 @@ require_once '../shortcuts_php/kobling.php'; ?>
  if (isset($_SESSION["brukerID"])) {
    echo "<div class='oversiktsmeny'>";
    echo "<div class='minprofil'>";
-    echo "<img src='../bilder/#'>";
-
-
+    echo "<img src='../bilder/profile.png' width='60px'>";
 
     $id = $_SESSION["brukerID"];
     $sql = "SELECT * FROM elev_login WHERE brukerID = $id";
@@ -36,20 +34,54 @@ require_once '../shortcuts_php/kobling.php'; ?>
 
 	}
   while ($rad = $resultat->fetch_assoc()) {
-  echo " $rad[navn]";
-
-   echo "</div>";
-   echo "<div class='fullførtkapitler'>";
-
-   echo "</div>";
-   echo "<div class='fullførtoppgaver'>";
-
-   echo "</div>";
-   echo "</div>";
- }
+  echo " <h3>$rad[navn]</h3>";
 }
+   echo "</div>";
 
+   echo "<div class='fullførtkapitler'>";
+   echo "<img src='../bilder/bøker.png' width='40px'>";
+   echo "<h3>Fullførte<br> kapitler: 2</h3>";
+   echo "</div>";
+
+   echo "<div class='fullførtoppgaver'>";
+   echo "<img src='../bilder/task.png' width='50px'>";
+   echo "<h3>Fullførte<br> oppgaver: 3</h3>";
+   echo "</div>";
+
+   echo "</div>";
+}
 ?>
+
+<div class="små_bokser">
+  <div class="øverste_rad">
+    <div class="liten_boks">
+        <a href="#"><img src="../bilder/html.png" width="100px"></a>
+        <a href="#"><img src="../bilder/css.png" width="100px"></a>
+    </div>
+    <div class="liten_boks">
+        <a href="#"><img src="../bilder/js.png" width="150px">
+    </div>
+    <div class="liten_boks">
+        <a href="#"><img src="../bilder/php-mysql.png" width="200px"></a>
+    </div>
+  </div>
+  <div class="andre_rad">
+    <div class="liten_boks_medtekst">
+        <a href="#"><img src="../bilder/spørsmålstegn.png" width="100px"></a>
+      <h4>Quiz og oppgaver</h4>
+    </div>
+    <div class="liten_boks_medtekst">
+        <a href="#"><img src="../bilder/forstørrelsesglass.png" width="100px"></a>
+      <h4>Oppslagsverk</h4>
+    </div>
+    <div class="liten_boks_medtekst">
+        <a href="#"><img src="../bilder/x.png" width="100px"></a>
+      <h4>Feilsøker</h4>
+    </div>
+  </div>
+</div>
+
+
       </div> <!-- Body-->
 
   </body>

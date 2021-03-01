be0be6d7537b6102cd3142d6894dda800047b2aa
Sindre <sindresa@viken.no>
2021-03-01 15:20:23 +0100

    Småendringer og opprydding i shortcuten header.php

--- shortcuts_php/header.php
+++ shortcuts_php/header.php
@@ -1,39 +1,45 @@
 
-  <body>
+<div class="header">
+    <img src="../bilder/strigologo.jfif" width="100px">
+    <h1>Strigo</h1>
 
-          <div class="body">
+<!--Header når logget inn-->
+      <?php
+        if (isset($_SESSION["brukerID"])) {
+            echo "<div class='knapper_innlogget'>";
+            echo "  <ul>";
+            echo "    <li><a href='../PHP/prfoil.php'>Min profil</a></li>";
+            echo "    <li><a href='../shortcuts_php/logout.inc.php'>Logg Ut</a></li>";
+            echo "  </ul>";
+            echo "</div>";
+          }
+      ?>
+</div>
 
-            <div class="header">
-              <img src="../bilder/strigologo.jfif" width="100px">
-              <h1>Strigo</h1>
-              <?php
-                if (isset($_SESSION["brukerID"])) {
-                  echo "<div class='knapper_innlogget'>";
-                  echo "<ul>";
-                  echo "  <li><a href='../PHP/prfoil.php'>Min profil</a></li>";
-                  echo "  <li><a href='../shortcuts_php/logout.inc.php'>Logg Ut</a></li>";
-                  echo "  </ul>";
-                }
-                ?>
-            </div>
-
+<!--Header når ikke logget inn-->
       <?php
         if (!isset($_SESSION["brukerID"])) {
-          echo "<ul>";
-          echo "<div class='knapper'>";
-          echo "<li><a href='../php/hovedside.php'>Praktisk info</a></li>";
-          echo " <li><a href='../php/bildegalleri.php'>For skolene</a></li>";
-          echo "<li><a href='../php/linkside.php'>For elever</a></li>";
-          echo "<li><a href='../php/gjestebok.php'><img src='../bilder/2.png' height='20px'></a></li>";
-          echo "<li><a href='../PHP/login.php'>Logg inn</a></li>";
-          echo "  </ul>";
-        }
-       ?>
+            echo "<div class='knapper_ikke_innlogget'>";
+            echo "  <ul>";
+            echo "    <li><a href='../php/hovedside.php'>Praktisk info</a></li>";
+            echo "    <li><a href='../php/bildegalleri.php'>For skolene</a></li>";
+            echo "    <li><a href='../php/linkside.php'>For elever</a></li>";
+            echo "    <li><a href='../php/gjestebok.php'><img src='../bilder/2.png' height='20px'></a></li>";
+            echo "    <li><a href='../PHP/login.php'>Logg inn</a></li>";
+            echo "  </ul>";
+            echo "</div>";
+          }
+      ?>
 
 
-  </div>
+
 
 <style>
+
+body {
+  background-color: #CFF3F7
+}
+
 .header {
   display: flex;
   flex-direction: row;
@@ -49,8 +55,21 @@
     align-items:center;
 }
 
-body {
-  background-color: #CFF3F7
+.knapper_ikke_innlogget{
+  display: flex;
+  justify-content: center;
+}
+
+.knapper_ikke_innlogget ul a {
+  width: 240px;
+  padding: 5px;
+  border-radius: 3px;
+  margin: 2px;
+  font-weight: bold;
+  font-size: 25px;
+  text-decoration: none;
+  background-color: #69b9c2;
+  color: white;
 }
 
 ul {
@@ -76,24 +95,8 @@ ul a {
   color: white;
 }
 
-.knapper_innlogget ul a {
-  width: 240px;
-  padding: 5px;
-  border-radius: 3px;
-  margin: 2px;
-  font-weight: bold;
-  font-size: 25px;
-  text-decoration: none;
-  background-color: #69b9c2;
-  color: white;
-}
-
 ul a:hover {
   background-color: #000010;
 }
 
-.knapper{
-  display: flex;
-  justify-content: center;
-}
 </style>

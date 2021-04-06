ae7d55b996c257f571c60b9a63634b905025feaa
AndreasMarken <andreasmar@viken.no>
2021-04-06 09:23:45 +0200

    fiksing av koden til ny struktur

--- shortcuts_php/togglemenu.php
+++ shortcuts_php/togglemenu.php
@@ -20,12 +20,12 @@
     <aside class="nav-sidebar">
       <ul>
         <li><span>Navigasjon</span></li>
-        <li><a href="../img/hovedside_innlogget.php">Hjemmeside</a></li>
-        <li><a href="../img/htmlogcsskurs.php">HTML + CSS</a></li>
-        <li><a href="../img/javakurs.php">Javascript</a></li>
-        <li><a href="../img/phpogmysqlkurs.php">PHP + Mysql</a></li>
-        <li><a href="../img/min_profil.php">Profil</a></li>
-        <li><a href="../img/404.php">Kontakt oss</a></li>
+        <li><a href="../pages/hovedside_innlogget.php">Hjemmeside</a></li>
+        <li><a href="../pages/htmlogcsskurs.php">HTML + CSS</a></li>
+        <li><a href="../pages/javakurs.php">Javascript</a></li>
+        <li><a href="../pages/phpogmysqlkurs.php">PHP + Mysql</a></li>
+        <li><a href="../pages/min_profil.php">Profil</a></li>
+        <li><a href="../pages/404.php">Kontakt oss</a></li>
       </ul>
     </aside>
 

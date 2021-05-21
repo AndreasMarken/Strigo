4291763523987c975f56e4dac26ffcf6455dfaac
Andreas <Andreas.Marken@gmail.com>
2021-05-21 23:33:07 +0200

    fikset slik at index funker for ny plassering

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
     <div class="navigation">
       <ul class="navigation__list">
-        <li class="navigation__item navigation__item--reekap"><a href="index.php"><img src="../img/reekap-logo.png" alt="Reekap"></a></li>
+        <li class="navigation__item navigation__item--reekap"><a href="index.php"><img src="img/reekap-logo.png" alt="Reekap"></a></li>
         <li class="navigation__item"><a href="pages/product.php" class="navigation__link">Our product</a></li>
         <li class="navigation__item"><a href="pages/pricing.php" class="navigation__link">Pricing</a></li>
         <li class="navigation__item"><a href="pages/contact.php" class="navigation__link">Contact</a></li>
@@ -50,8 +50,8 @@
   <svg width="414" height="420" viewBox="0 0 414 420" xmlns="http://www.w3.org/2000/svg" class="splash--1">
 
     <path fill-rule="evenodd" clip-rule="evenodd" d="M486.947 272.5C486.513 310.166 558.228 319.644 559.783 357.281C561.161 390.644 516.711 406.92 493.491 430.916C472.923 452.172 451.169 470.85 429.949 491.454C403.434 517.198 384.617 551.335 351.856 568.436C315.183 587.579 272.885 600.917 231.972 594.797C190.282 588.561 134.737 574.504 122.585 534.14C106.932 482.146 160.829 433.229 167.093 379.292C169.883 355.267 163.521 328.996 148.622 309.944C106.493 256.069 19.9594 235.146 2.63661 168.985C-10.038 120.577 28.2198 40.9748 78.2591 41.2063C157.99 41.5751 196.967 175.181 276.546 170.252C331.558 166.844 305.951 56.8941 350.433 24.3481C388.437 -3.45844 447.795 -7.07016 489.795 14.2258C531.332 35.2867 562.894 85.2457 562.38 131.814C561.787 185.61 487.567 218.705 486.947 272.5Z" fill="#F5B78A"/>
-    <a href="#"><img src="../img/image 3.png" class="abs-right--2"></a>
-    <a href="#"><img src="../img/image 4.png" class="abs-right"></a>
+    <a href="#"><img src="img/image 3.png" class="abs-right--2"></a>
+    <a href="#"><img src="img/image 4.png" class="abs-right"></a>
 
   </svg>
 
